c5aeb454726c7e54967a5c9f8e3a7513e559689b
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-08-25 09:47:41 +0300

    commit tree ba1b54f3b97ca1c6bfd0296ff33cb9867ac68623

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-
-end
+    # setter method
+    def name=(name)
+       @name = name
+     end
+   
+     # getter method
+     def name
+       @name
+     end
+   
+     def job=(jobs)
+       @job = jobs
+     end
+   
+     # getter method
+     def job
+       @job
+     end
+   end 
